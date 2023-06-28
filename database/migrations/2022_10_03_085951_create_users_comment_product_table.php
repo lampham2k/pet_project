3e416d5cd1eaa1331c538f05f5cb0cc8e3baf821
Phạm Quốc Lâm <110162903+PQLam@users.noreply.github.com>
2023-06-28 17:23:54 +0700

    edit name table user comment products

--- database/migrations/2022_10_03_085951_create_users_comment_product_table.php
+++ database/migrations/2022_10_03_085951_create_users_comment_product_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users_comment_products', function (Blueprint $table) {
+        Schema::create('user_comment_products', function (Blueprint $table) {
             $table->id()->unsigned();
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId('product_id')->constrained('products');
